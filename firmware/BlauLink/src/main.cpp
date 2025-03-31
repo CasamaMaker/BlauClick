6817289ed653a57195d7b69f1cf6fffdd3e14601
RaspiGame <91.marcas@gmail.com>
2025-03-31 20:20:18 +0200

    restored pass version

--- firmware/BlauLink/src/main.cpp
+++ firmware/BlauLink/src/main.cpp
@@ -10,12 +10,20 @@
 #include "ESPAsyncWebServer.h"
 #include "DNSServer.h"
 // #include <wifimanager.h>
-// #include "SPIFFS.h"
+#include "SPIFFS.h"
 // #include "spiffs.h"
-#include <LittleFS.h>
+// #include <LittleFS.h>
 #include <esp_sleep.h>
 #include <EEPROM.h>
 #include <esp_now.h>
+
+
+#define enVBatterySense 0
+#define Boto 1
+#define enBoto 4
+#define digitalLed 5
+
+
 
 const char* ssid = "EspLink-AP"; //Name of the WIFI network hosted by the device
 const char* password =  "";               //Password
@@ -33,7 +41,7 @@ const char* macPath = "/mac.txt"; // File paths to save input values permanently
 //****************** DIGITAL LED ******************************
 #include <FastLED.h>
 #define NUM_LEDS 1
-#define DATA_PIN 6
+#define DATA_PIN digitalLed //6
 #define BRIGHTNESS  15
 CRGB leds[NUM_LEDS];
 
@@ -59,6 +67,28 @@ typedef struct {
 struct_message missatge;
 
 // esp_now_peer_info_t peerInfo;
+
+
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  // Serial.println(status);
+  if(status == ESP_NOW_SEND_SUCCESS){
+    Serial.println("Delivery Success" + status);
+    leds[0] = CRGB::Green;
+    FastLED.show();
+    // leds[0] = CRGB::Green;
+    // FastLED.show();
+  }else{
+    Serial.println("Delivery Fail" + status);
+    leds[0] = CRGB::Red;
+    FastLED.show();
+    // leds[0] = CRGB::Red;
+    // FastLED.show();
+  }
+}
 
 void config_ESPNOW(){
   WiFi.mode(WIFI_STA);
@@ -89,26 +119,6 @@ void send_ESPNOW(){
   esp_err_t result = esp_now_send(receiverMac, (uint8_t *) &missatge, sizeof(missatge));  //broadcastAddress, (uint8_t *) &missatge, sizeof(missatge));
   if (result != ESP_OK) {
     Serial.println("Error al enviar el dato");
-  }
-}
-
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  // Serial.println(status);
-  if(status == ESP_NOW_SEND_SUCCESS){
-    Serial.println("Delivery Success" + status);
-    leds[0] = CRGB::Green;
-    FastLED.show();
-    // leds[0] = CRGB::Green;
-    // FastLED.show();
-  }else{
-    Serial.println("Delivery Fail" + status);
-    leds[0] = CRGB::Red;
-    FastLED.show();
-    // leds[0] = CRGB::Red;
-    // FastLED.show();
   }
 }
 
@@ -150,6 +160,7 @@ String* scanNetworks() {
   }
 
   // Retorna l'array amb les adreces MAC trobades
+  // Serial.printf(*macAddresses);
   return macAddresses;
 }
 
@@ -160,7 +171,7 @@ void webServerSetup(){
 
   // accedeix aquí just conectar-se a la wifi des de l'ordinador
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/wifimanager.html", "text/html");
+    request->send(SPIFFS, "/wifimanager.html", "text/html");
     Serial.println("myweb /");
   });
 
@@ -168,31 +179,33 @@ void webServerSetup(){
   //This is an example of triggering for a known location.  This one seems to be common for android devices
   server.on("/generate_204", HTTP_GET, [](AsyncWebServerRequest *request){
     //request->send(200, "text/plain", "You were sent here by a captive portal after requesting generate_204");
-    request->send(LittleFS, "/wifimanager.html", "text/html");
+    request->send(SPIFFS, "/wifimanager.html", "text/html");
     Serial.println("requested /generate_204");
   });
 
   // accedeix aquí quan busques qualsevol web al navegador
   //This is an example of a redirect type response.  onNotFound acts as a catch-all for any request not defined above
   server.onNotFound([](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/wifimanager.html", "text/html"); //request->redirect("/");
+    request->send(SPIFFS, "/wifimanager.html", "text/html"); //request->redirect("/");
     Serial.print("server.notfound triggered: ");
     Serial.println(request->url());       //This gives some insight into whatever was being requested
   });
 
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/style.css", "text/css");
+    request->send(SPIFFS, "/style.css", "text/css");
     Serial.println("Served CSS");
   });
 
   server.on("/mac", HTTP_GET, [](AsyncWebServerRequest *request) {
       request->send(200, "text/plain", mac); //String(mac).c_str());
+      Serial.println(mac);
   });
 
   // Ruta per obtenir la llista d'adreces MAC
   server.on("/macList", HTTP_GET, [](AsyncWebServerRequest *request) {
     String macListStr = "";  // Crear una cadena per a les adreces MAC
     String* macList = scanNetworks();  // Crida a la funció per obtenir les adreces MAC
+    Serial.println(*macList);
     for (int i = 0; i < MAX_NETWORKS; i++) {
       if (macList[i] != "") {  // Només afegir adreces MAC no buides
         macListStr += macList[i];  // Afegir l'adreça MAC a la cadena
@@ -253,10 +266,12 @@ void webServerSetup(){
 void setup() {
   startTime = millis(); // Guarda el temps actual en mil·lisegons al iniciar
   Serial.begin(115200);
+  // delay(1000);
+  Serial.println("Aaaaaaaaaaaaaaaaaaaa");
   EEPROM.begin(512);
  //******************** SPIFFS ***********************
   // initSPIFFS();
-  if (!LittleFS.begin(true)) {
+  if (!SPIFFS.begin()) {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
   //mac = EEPROM.read(0);
@@ -274,11 +289,13 @@ void setup() {
       broadcastAddress[i] = receiverMac[i];
   }*/
 
-  pinMode(5, INPUT);  // boto
+  pinMode(Boto, INPUT);  // 5 boto
+  pinMode(enBoto, OUTPUT);
+  digitalWrite(enBoto, HIGH);
 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  leds[0] = CRGB::Blue;
-  FastLED.show();
+  // leds[0] = CRGB::Blue;
+  // FastLED.show();
 
   config_ESPNOW();
   // WiFi.mode(WIFI_STA);
@@ -316,25 +333,27 @@ void setup() {
   // if (result != ESP_OK) {
   //   Serial.println("Error al enviar el dato");
   // }
+  delay(100);
 }
 
 
 void loop() {
   //dnsServer.processNextRequest();         //Without this, the connected device will simply timeout trying to reach the internet
                                           //or it might fall back to mobile data if it has it
-  if(digitalRead(5)){
-  leds[0] = CRGB::Blue;
-  FastLED.show();
-  leds[0] = CRGB::Blue;
-  FastLED.show();
-  delay(100);
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(100);
+  if(digitalRead(Boto)){
+    leds[0] = CRGB::Green;
+    FastLED.show();
+    // leds[0] = CRGB::Blue;
+    // FastLED.show();
+    delay(100);
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(100);
   
   }else{
     delay(50);
-    esp_deep_sleep_start();
+    // esp_deep_sleep_start();
+    digitalWrite(enBoto, LOW);
   }
 
 
@@ -342,8 +361,8 @@ void loop() {
     //mac = EEPROM.read(0);//readFile(SPIFFS, macPath);
     Serial.print("MAC guardada:  "); Serial.println(mac);
 
-    leds[0] = CRGB::Red;
-    FastLED.show();
+    // leds[0] = CRGB::Red;
+    // FastLED.show();
     leds[0] = CRGB::Red;
     FastLED.show();
     WiFi.softAP(ssid, password);            //This starts the WIFI radio in access point mode
@@ -355,22 +374,42 @@ void loop() {
 
     bool buttonStateLow1=false;
     bool buttonStateHigh2=false;
+    bool buttonReleased = false;
     while(1){
       dnsServer.processNextRequest();
       if(startTime + 60000 < millis()){
-        esp_deep_sleep_start();
+        // esp_deep_sleep_start();
+        digitalWrite(enBoto, LOW);
       }
 
       // detecta si s'ha tornat a presionar el boto, per aleshores apagar
-      if(!digitalRead(5) || buttonStateLow1){
-        buttonStateLow1=true;
-        if(digitalRead(5) || buttonStateHigh2){
-          buttonStateHigh2 = true;
-          if(!digitalRead(5)){
-            esp_deep_sleep_start();
-          }
-        }
+      // if(!digitalRead(5) || buttonStateLow1){
+      //   buttonStateLow1=true;
+      //   if(digitalRead(5) || buttonStateHigh2){
+      //     buttonStateHigh2 = true;
+      //     if(!digitalRead(5)){
+      //       esp_deep_sleep_start();
+      //     }
+      //   }
+      // }
+
+      static bool lastButtonState = HIGH;  // Estat anterior del botó
+
+      bool buttonState = digitalRead(Boto);  // Llegeix el botó
+
+      if (buttonState == LOW && lastButtonState == HIGH && !buttonReleased) {
+          buttonReleased = true;  // Marquem que s'ha alliberat el botó
+          Serial.println("Botó alliberat");
+          delay(200);
       }
+
+      if (buttonState == HIGH && lastButtonState == LOW && buttonReleased) {
+          Serial.println("Botó premut després d'alliberar");
+          buttonReleased = false;  // Reiniciem per detectar una nova seqüència
+          delay(200);
+          digitalWrite(enBoto, LOW);
+      }
+      lastButtonState = buttonState;  // Guardem l'estat per a la següent iteració
 
     }
   }
